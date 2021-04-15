8ec71ce110e7d3042b0322671d29c7d14b11eb75
ChadDiaz <chadcdiaz@gmail.com>
2021-04-15 15:56:36 -0400

    setting up server and knex enviornments

---
+++ knexfile.js
@@ -0,0 +1,58 @@
+// Update with your config settings.
+const pgConnection = process.env.DATABASE_URL;
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+    connection: {
+      filename: "./data.pasta.db3",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+  },
+
+  testing: {
+    client: sqlite3,
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+    connection: {
+      filename: "./data/testing.db3",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
