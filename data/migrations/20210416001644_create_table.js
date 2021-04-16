796457430cb1b688e9f98e88df6543905278603d
ChadDiaz <chadcdiaz@gmail.com>
2021-04-16 14:25:23 -0400

    added 2nd table for users to start building a users auth file

--- data/migrations/20210416001644_create_table.js
+++ data/migrations/20210416001644_create_table.js
@@ -1,10 +1,23 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("pastas", (tbl) => {
-    tbl.increments();
-    tbl.string("pasta_name", 128).unique().notNullable();
-  });
+  return knex.schema
+    .createTable("pastas", (tbl) => {
+      tbl.increments("pasta_id");
+      tbl.string("pasta_name", 128).unique().notNullable();
+    })
+    .createTable("users", (tbl) => {
+      tbl.increments("user_id");
+      tbl.string("username", 128).notNullable().unique();
+      tbl.string("password", 128).notNullable();
+      tbl
+        .integer("pasta_id")
+        .unsigned()
+        .notNullable()
+        .references("pastas.pasta_id")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("pastas");
+  return knex.schema.dropTableIfExists("users").dropTableIfExists("pastas");
 };
