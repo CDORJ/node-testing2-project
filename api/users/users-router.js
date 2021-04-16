9a9894e4c800e9f79c10fd17d2cebcb869219ec2
ChadDiaz <chadcdiaz@gmail.com>
2021-04-16 16:03:48 -0400

    first router-test successful after building router. did test driven development method

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,13 @@
 const router = require("express").Router();
 const User = require("./users-model.js");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await User.getUser();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
--- api/users/users-router.spec.js
+++ api/users/users-router.spec.js
@@ -1,3 +1,39 @@
-const Users = require('./users-model.js');
-const server = require('../server.js')
-const request = require('supertest');
+const Users = require("./users-model.js");
+const server = require("../server.js");
+const request = require("supertest");
+const db = require("../../data/dbConfig.js");
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db("users").truncate();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+describe("testing the auth router file", () => {
+  describe("GET endpoint", () => {
+    test('router.get("/") returns list of users', async () => {
+      await Users.registerUser({ username: "chad", password: "password" });
+      const users = await request(server).get("/api/users");
+      console.log(users.body);
+      expect(users.body).toEqual([
+        { user_id: 1, username: "chad", password: "password" },
+      ]);
+    });
+    test("response status should be 200", async () => {
+      await Users.registerUser({
+        username: "chad",
+        password: "password",
+      });
+      const users = await request(server).get("/api/users");
+      expect(users.status).toBe(200);
+    });
+  });
+  describe("POST ENDPOINTS", () => {});
+});
